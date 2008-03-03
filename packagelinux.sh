33d705e89422a63c1e056165babe13cada97f0ab
austin.goudge <aussig@users.noreply.github.com>
2008-03-03 22:15:48 +0000

    Added Linux and Windows packaging scripts

---
+++ packagelinux.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Create a tarball of XAddonManager but exclude resource forks
+# Mac OS 10.4 and earlier: export COPY_EXTENDED_ATTRIBUTES_DISABLE=true
+export COPYFILE_DISABLE=true
+tar -cf XAddonManager\ Linux.tar XAddonManager
+gzip XAddonManager\ Linux.tar
