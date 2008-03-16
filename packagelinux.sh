5b9d0c4c74899bbf3bd8f83f33b6d4e9905ee67b
austin.goudge <aussig@users.noreply.github.com>
2008-03-16 19:31:03 +0000

    Modified packaging scripts to use version number

--- packagelinux.sh
+++ packagelinux.sh
@@ -3,5 +3,5 @@
 # Create a tarball of XAddonManager but exclude resource forks
 # Mac OS 10.4 and earlier: export COPY_EXTENDED_ATTRIBUTES_DISABLE=true
 export COPYFILE_DISABLE=true
-tar -cf XAddonManager\ Linux.tar XAddonManager
-gzip XAddonManager\ Linux.tar
+tar -cf XAddonManager-x.x.x-Linux.tar XAddonManager
+gzip XAddonManager_Linux.tar
--- packagewin.sh
+++ packagewin.sh
@@ -3,4 +3,4 @@
 # Create a zip of XAddonManager but exclude resource forks
 # Mac OS 10.4 and earlier: export COPY_EXTENDED_ATTRIBUTES_DISABLE=true
 export COPYFILE_DISABLE=true
-zip -r XAddonManager\ Windows.zip XAddonManager
+zip -r XAddonManager-x.x.x-Windows.zip XAddonManager
