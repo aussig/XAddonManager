b9158c1f46742f36fa7635bfe5ec435f6e64d67f
austin.goudge <aussig@users.noreply.github.com>
2008-03-21 10:29:01 +0000

    Fixed Linux packaging script

--- packagelinux.sh
+++ packagelinux.sh
@@ -4,4 +4,4 @@
 # Mac OS 10.4 and earlier: export COPY_EXTENDED_ATTRIBUTES_DISABLE=true
 export COPYFILE_DISABLE=true
 tar -cf XAddonManager-x.x.x-Linux.tar XAddonManager
-gzip XAddonManager_Linux.tar
+gzip XAddonManager-x.x.x-Linux.tar
