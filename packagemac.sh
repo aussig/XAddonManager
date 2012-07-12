163092daa407bc275690a93b3c510f0817046869
austin.goudge <aussig@users.noreply.github.com>
2012-07-12 17:06:37 +0000

    Mac packaging script now signs the code

--- packagemac.sh
+++ packagemac.sh
@@ -9,7 +9,16 @@ echo ''
 echo 'Building Mac Package '$VERSION
 echo '======================================'
 
-cd 'Builds - XAddonManager.rbvcp/Mac OS X (Universal)'
+cd 'Builds - XAddonManager.rbvcp/Mac OS X (Intel)'
+
+# Code signing
+/usr/libexec/PlistBuddy -c "Delete :CFBundleIdentifier" XAddonManager.app/Contents/Info.plist
+/usr/libexec/PlistBuddy -c "Delete :NSHumanReadableCopyright" XAddonManager.app/Contents/Info.plist
+/usr/libexec/PlistBuddy -c "Delete :LSApplicationCategoryType" XAddonManager.app/Contents/Info.plist
+/usr/libexec/PlistBuddy -c "Add :CFBundleIdentifier string 'com.AustinGoudge.XAddonManager'" XAddonManager.app/Contents/Info.plist
+/usr/libexec/PlistBuddy -c "Add :NSHumanReadableCopyright string '© 2012, Austin Goudge'" XAddonManager.app/Contents/Info.plist
+/usr/libexec/PlistBuddy -c "Add :LSApplicationCategoryType string 'public.app-category.simulation-games'" XAddonManager.app/Contents/Info.plist
+codesign --signature-size 6400 -f -v -s 'Austin Goudge' XAddonManager.app
 
 zip -r XAddonManager-$VERSION-Mac.zip XAddonManager.app
 mv XAddonManager-$VERSION-Mac.zip ../../XAddonManager-$VERSION-Mac.zip
